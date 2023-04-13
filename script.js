d912e1e58cef538e33ac78d2b9ef994e4a59e612
Maks <killingtime2101@mail.ru>
2023-04-13 23:22:23 +0700

    добавлен функцционал постов и поиска

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const USER_URL = 'https://jsonplaceholder.typicode.com/users'
-const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts?userId='
 
 const dataContainer = document.querySelector('#data-container');
 
-const createUserElement = (text) => {
+const createUserElement = (text, id) => {
     const userElementList = document.createElement('ul');
     const userElement = document.createElement('li');
+    const userBtn = document.createElement('button');
+
     userElement.textContent = text;
-    userElementList.append(userElement);
+    userBtn.textContent = 'Показать посты';
+    userBtn.dataset.id = id;
+    userBtn.classList.add('user-post-button');
+    userElementList.append(userElement, userBtn);
 
     return userElementList;
+}
+
+const createUserPosts = (title, body) => {
+    const postList = document.createElement('li');
+    const postTitle = document.createElement('h4');
+    const postBody = document.createElement('p');
+
+    postTitle.textContent = title;
+    postBody.textContent = body;
+
+    postList.append(postTitle, postBody)
+    return postList;
 }
 
 const loader = () => {
@@ -22,14 +39,45 @@ const loader = () => {
     }
 }
 
+async function openPosts(event) {
+    const button = event.target;
+    const userId = button.dataset.id;
+    const userElement = button.parentElement;
+    const postList = userElement.querySelector('.user-post-list');
+
+    if (postList) {
+        postList.remove();
+        button.textContent = 'Показать посты';
+    } else {
+        try {
+            const posts = await getUserPosts(userId);
+            const userPostList = document.createElement('ul');
+            userPostList.classList.add('user-post-list');
+
+            posts.forEach((post) => {
+                const userPost = createUserPosts(post.title, post.body);
+                userPostList.append(userPost);
+            });
+
+            userElement.append(userPostList);
+            button.textContent = 'Скрыть посты';
+        } catch (error) {
+            console.error(error);
+        }
+    }
+};
+
 function getUsers() {
     loader();
-    return fetch(USER_URL)
+    fetch(USER_URL)
         .then(response => response.json())
         .then((users) => {
             users.forEach((user) => {
-                const userHTML = createUserElement(user.name);
+                const userId = user.id
+                const userHTML = createUserElement(user.name, userId);
                 dataContainer.append(userHTML);
+
+                userHTML.querySelector('.user-post-button').addEventListener('click', openPosts);
             });
         })
         .catch((error) => console.log(error))
@@ -41,14 +89,30 @@ async function getUserPosts(userId) {
     if (!userId) {
         throw new Error('Id Пользователя не найден')
     }
-    return fetch(`${POSTS_URL}/${userId}`)
+    return fetch(`${POSTS_URL}${userId}`)
 
         .then(response => response.json())
         .then((posts) => {
-            console.log(posts)
+            return posts;
         })
-
 };
 
-// getUserPosts();
+const userSearchInput = document.getElementById('search-input');
+
+function filterUsers() {
+    const searchName = userSearchInput.value.toLowerCase();
+    const userElements = document.querySelectorAll('#data-container > ul');
+
+    userElements.forEach((userElement) => {
+        const userName = userElement.querySelector('li').textContent.toLowerCase();
+        if (userName.includes(searchName)) {
+            userElement.removeAttribute('hidden');
+        } else {
+            userElement.setAttribute('hidden', '');
+        }
+    });
+}
+
+userSearchInput.addEventListener('input', filterUsers);
+
 getUsers();
